cf53fd86c086e82c110ce5f81c2ba42e867a0549
root <root@localhost.localdomain>
2021-06-09 09:51:24 +0530

    1 to 100

---
+++ Without_using_number_print_1to100.java
@@ -0,0 +1,14 @@
+class Without_using_number_print_1to100
+{
+    public static void main(String[] args) {
+        String a="..........";
+        String b=".";
+        int c=b.length();
+        int d=a.length();
+        for(int e=c;e<=d*d;e++)
+        {
+            System.out.println(e);
+        }
+        
+    }
+}
